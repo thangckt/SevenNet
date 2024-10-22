5dfd7d870c8ca2f1882e7f0b17ffbc8612356cd5
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-10-23 03:43:33 +0900

    Update util.py

--- sevenn/util.py
+++ sevenn/util.py
@@ -23,9 +23,7 @@ def to_atom_graph_list(atom_graph_batch):
     indices = atom_graph_batch[KEY.NUM_ATOMS].tolist()
 
     atomic_energy_list = torch.split(atom_graph_batch[KEY.ATOMIC_ENERGY], indices)
-    inferred_total_energy_list = torch.unbind(
-        atom_graph_batch[KEY.PRED_TOTAL_ENERGY]
-    )
+    inferred_total_energy_list = torch.unbind(atom_graph_batch[KEY.PRED_TOTAL_ENERGY])
     inferred_force_list = torch.split(atom_graph_batch[KEY.PRED_FORCE], indices)
 
     inferred_stress_list = None
@@ -55,18 +53,18 @@ def error_recorder_from_loss_functions(loss_functions):
         name = loss_function.name
         base = None
         if type(loss_function) is PerAtomEnergyLoss:
-            base = get_err_type('Energy')
+            base = get_err_type("Energy")
         elif type(loss_function) is ForceLoss:
-            base = get_err_type('Force')
+            base = get_err_type("Force")
         elif type(loss_function) is StressLoss:
-            base = get_err_type('Stress')
+            base = get_err_type("Stress")
         else:
             base = {}
-        base['name'] = name
-        base['ref_key'] = ref_key
-        base['pred_key'] = pred_key
+        base["name"] = name
+        base["ref_key"] = ref_key
+        base["pred_key"] = pred_key
         if type(criterion) is torch.nn.MSELoss:
-            base['name'] = base['name'] + '_RMSE'
+            base["name"] = base["name"] + "_RMSE"
             metrics.append(RMSError(**base))
         elif type(criterion) is torch.nn.L1Loss:
             metrics.append(MAError(**base))
@@ -107,7 +105,7 @@ def postprocess_output(output, loss_types):
             ref = torch.reshape(output[KEY.STRESS] * TO_KB, (-1,))
             vdim = 6
         else:
-            raise ValueError(f'Unknown loss type: {loss_type}')
+            raise ValueError(f"Unknown loss type: {loss_type}")
         results[loss_type] = (pred, ref, vdim)
     return results
 
@@ -121,14 +119,14 @@ def onehot_to_chem(one_hot_indices: List[int], type_map: Dict[int, int]):
 
 def _patch_old_config(config: Dict[str, Any]):
     # Fixing my old mistakes
-    if config[KEY.CUTOFF_FUNCTION][KEY.CUTOFF_FUNCTION_NAME] == 'XPLOR':
-        config[KEY.CUTOFF_FUNCTION].pop('poly_cut_p_value', None)
+    if config[KEY.CUTOFF_FUNCTION][KEY.CUTOFF_FUNCTION_NAME] == "XPLOR":
+        config[KEY.CUTOFF_FUNCTION].pop("poly_cut_p_value", None)
     if KEY.TRAIN_DENOMINTAOR not in config:
-        config[KEY.TRAIN_DENOMINTAOR] = config.pop('train_avg_num_neigh', False)
-    _opt = config.pop('optimize_by_reduce', None)
+        config[KEY.TRAIN_DENOMINTAOR] = config.pop("train_avg_num_neigh", False)
+    _opt = config.pop("optimize_by_reduce", None)
     if _opt is False:
         raise ValueError(
-            'This checkpoint(optimize_by_reduce: False) is no longer supported'
+            "This checkpoint(optimize_by_reduce: False) is no longer supported"
         )
     if KEY.CONV_DENOMINATOR not in config:
         config[KEY.CONV_DENOMINATOR] = 0.0
@@ -144,29 +142,27 @@ def _map_old_model(old_model_state_dict):
     Map old model's module names to new model's module names
     """
     _old_module_name_mapping = {
-        'EdgeEmbedding': 'edge_embedding',
-        'reducing nn input to hidden': 'reduce_input_to_hidden',
-        'reducing nn hidden to energy': 'reduce_hidden_to_energy',
-        'rescale atomic energy': 'rescale_atomic_energy',
+        "EdgeEmbedding": "edge_embedding",
+        "reducing nn input to hidden": "reduce_input_to_hidden",
+        "reducing nn hidden to energy": "reduce_hidden_to_energy",
+        "rescale atomic energy": "rescale_atomic_energy",
     }
     for i in range(10):
-        _old_module_name_mapping[f'{i} self connection intro'] = (
-            f'{i}_self_connection_intro'
+        _old_module_name_mapping[f"{i} self connection intro"] = (
+            f"{i}_self_connection_intro"
         )
-        _old_module_name_mapping[f'{i} convolution'] = f'{i}_convolution'
-        _old_module_name_mapping[f'{i} self interaction 2'] = (
-            f'{i}_self_interaction_2'
-        )
-        _old_module_name_mapping[f'{i} equivariant gate'] = f'{i}_equivariant_gate'
+        _old_module_name_mapping[f"{i} convolution"] = f"{i}_convolution"
+        _old_module_name_mapping[f"{i} self interaction 2"] = f"{i}_self_interaction_2"
+        _old_module_name_mapping[f"{i} equivariant gate"] = f"{i}_equivariant_gate"
 
     new_model_state_dict = {}
     for k, v in old_model_state_dict.items():
-        key_name = k.split('.')[0]
-        follower = '.'.join(k.split('.')[1:])
-        if 'denumerator' in follower:
-            follower = follower.replace('denumerator', 'denominator')
+        key_name = k.split(".")[0]
+        follower = ".".join(k.split(".")[1:])
+        if "denumerator" in follower:
+            follower = follower.replace("denumerator", "denominator")
         if key_name in _old_module_name_mapping:
-            new_key_name = _old_module_name_mapping[key_name] + '.' + follower
+            new_key_name = _old_module_name_mapping[key_name] + "." + follower
             new_model_state_dict[new_key_name] = v
         else:
             new_model_state_dict[k] = v
@@ -178,20 +174,20 @@ def model_from_checkpoint(checkpoint) -> Tuple[torch.nn.Module, Dict]:
     from .model_build import build_E3_equivariant_model
 
     if isinstance(checkpoint, str):
-        checkpoint = torch.load(checkpoint, map_location='cpu', weights_only=False)
+        checkpoint = torch.load(checkpoint, map_location="cpu", weights_only=False)
     elif isinstance(checkpoint, dict):
         pass
     else:
-        raise ValueError('checkpoint must be either str or dict')
+        raise ValueError("checkpoint must be either str or dict")
 
-    model_state_dict = checkpoint['model_state_dict']
-    config = checkpoint['config']
+    model_state_dict = checkpoint["model_state_dict"]
+    config = checkpoint["config"]
     defaults = {**model_defaults(config)}
     config = _patch_old_config(config)
 
     for k, v in defaults.items():
         if k not in config:
-            warnings.warn(f'{k} not in config, using default value {v}', UserWarning)
+            warnings.warn(f"{k} not in config, using default value {v}", UserWarning)
             config[k] = v
 
     # expect only non-tensor values in config, if exists, move to cpu
@@ -208,9 +204,9 @@ def model_from_checkpoint(checkpoint) -> Tuple[torch.nn.Module, Dict]:
         updated = _map_old_model(model_state_dict)
         missing, not_used = model.load_state_dict(updated, strict=False)
         if len(not_used) > 0:
-            warnings.warn(f'Some keys are not used: {not_used}', UserWarning)
+            warnings.warn(f"Some keys are not used: {not_used}", UserWarning)
 
-    assert len(missing) == 0, f'Missing keys: {missing}'
+    assert len(missing) == 0, f"Missing keys: {missing}"
 
     return model, config
 
@@ -219,9 +215,7 @@ def unlabeled_atoms_to_input(atoms, cutoff: float, grad_key: str = KEY.EDGE_VEC)
     from .atom_graph_data import AtomGraphData
     from .train.dataload import unlabeled_atoms_to_graph
 
-    atom_graph = AtomGraphData.from_numpy_dict(
-        unlabeled_atoms_to_graph(atoms, cutoff)
-    )
+    atom_graph = AtomGraphData.from_numpy_dict(unlabeled_atoms_to_graph(atoms, cutoff))
     atom_graph[grad_key].requires_grad_(True)
     atom_graph[KEY.BATCH] = torch.zeros([0])
     return atom_graph
@@ -260,7 +254,7 @@ def dtype_correct(
         if np.issubdtype(v.dtype, np.floating):
             return torch.from_numpy(v).to(float_dtype)
         elif np.issubdtype(v.dtype, np.integer):
-            return torch.from_numpy(v).to(int_dtype)
+            return torch.from_numpy(np.asarray(v)).to(int_dtype)
     elif isinstance(v, torch.Tensor):
         if v.dtype.is_floating_point:
             return v.to(float_dtype)  # convert to specified float dtype
@@ -279,10 +273,10 @@ def infer_irreps_out(
     irreps_x: Irreps,
     irreps_operand: Irreps,
     drop_l: Union[bool, int] = False,
-    parity_mode: str = 'full',
+    parity_mode: str = "full",
     fix_multiplicity: Union[bool, int] = False,
 ):
-    assert parity_mode in ['full', 'even', 'sph']
+    assert parity_mode in ["full", "even", "sph"]
     # (mul, (ir, p))
     irreps_out = FullTensorProduct(irreps_x, irreps_operand).irreps_out.simplify()
     new_irreps_elem = []
@@ -290,9 +284,9 @@ def infer_irreps_out(
         elem = (mul, (l, p))
         if drop_l is not False and l > drop_l:
             continue
-        if parity_mode == 'even' and p == -1:
+        if parity_mode == "even" and p == -1:
             continue
-        elif parity_mode == 'sph' and p != (-1) ** l:
+        elif parity_mode == "sph" and p != (-1) ** l:
             continue
         if fix_multiplicity:
             elem = (fix_multiplicity, (l, p))
@@ -304,14 +298,14 @@ def pretrained_name_to_path(name: str) -> str:
     import sevenn._const as _const
 
     name = name.lower()
-    heads = ['sevennet-0', '7net-0']
+    heads = ["sevennet-0", "7net-0"]
     checkpoint_path = None
-    if name in [f'{n}_11july2024' for n in heads] or name in heads:
+    if name in [f"{n}_11july2024" for n in heads] or name in heads:
         checkpoint_path = _const.SEVENNET_0_11July2024
-    elif name in [f'{n}_22may2024' for n in heads]:
+    elif name in [f"{n}_22may2024" for n in heads]:
         checkpoint_path = _const.SEVENNET_0_22May2024
     else:
-        raise ValueError('Not a valid potential')
+        raise ValueError("Not a valid potential")
 
     return checkpoint_path
 
@@ -324,23 +318,23 @@ def unique_filepath(filepath: str) -> str:
         fname = os.path.basename(filepath)
         name, ext = os.path.splitext(fname)
         cnt = 0
-        new_name = f'{name}{cnt}{ext}'
+        new_name = f"{name}{cnt}{ext}"
         new_path = os.path.join(dirname, new_name)
         while os.path.exists(new_path):
             cnt += 1
-            new_name = f'{name}{cnt}{ext}'
+            new_name = f"{name}{cnt}{ext}"
             new_path = os.path.join(dirname, new_name)
         return new_path
 
 
 def get_error_recorder(
     recorder_tuples: List[Tuple[str, str]] = [
-        ('Energy', 'RMSE'),
-        ('Force', 'RMSE'),
-        ('Stress', 'RMSE'),
-        ('Energy', 'MAE'),
-        ('Force', 'MAE'),
-        ('Stress', 'MAE'),
+        ("Energy", "RMSE"),
+        ("Force", "RMSE"),
+        ("Stress", "RMSE"),
+        ("Energy", "MAE"),
+        ("Force", "MAE"),
+        ("Stress", "MAE"),
     ],
 ):
     # TODO add criterion argument and loss recorder selections
@@ -350,7 +344,7 @@ def get_error_recorder(
     err_metrics = []
     for err_type, metric_name in config:
         metric_kwargs = error_recorder.get_err_type(err_type).copy()
-        metric_kwargs['name'] += f'_{metric_name}'
+        metric_kwargs["name"] += f"_{metric_name}"
         metric_cls = error_recorder.ErrorRecorder.METRIC_DICT[metric_name]
         err_metrics.append(metric_cls(**metric_kwargs))
     return error_recorder.ErrorRecorder(err_metrics)
